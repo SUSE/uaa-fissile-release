89b76d4ea6eb1f64e2a74fb229cdb29489a815ac
Mark Yen <mark.yen@suse.com>
2017-09-29 17:07:49 -0700

    create-release.sh: Code review changes
    - Reduce how much we touch CDPATH (to be clearer)
    - Drop --interactive on docker invocation
    
    Addresses:
    - https://github.com/SUSE/uaa-fissile-release/pull/47/files#r141987713
    - https://github.com/SUSE/uaa-fissile-release/pull/47/files#r141988293

--- bin/create-release.sh
+++ bin/create-release.sh
@@ -2,12 +2,7 @@
 set -o errexit
 set -o nounset
 
-# prevent cd from printing the directory it changes to. This breaks
-# cd/pwd constructions (See **).
-unset CDPATH
-
-# (**)
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
+ROOT="$( unset CDPATH ; cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 
 if [[ $# -lt 2 || -z "${1:-}" || -z "${2:-}" ]]; then
   cat <<HELP
@@ -110,7 +105,6 @@ rm -rf ${ROOT}/${release_path}/dev_releases
 if find "${ROOT}/${release_path}/blobs/" -type l >/dev/null 2>/dev/null; then
   # Migrating from BOSH CLI v1 (ruby) to BOSH CLI v2 (golang)
   docker run \
-    --interactive \
     --rm \
     --volume "${FISSILE_CACHE_DIR}":/bosh-cache \
     --volume "${ROOT}/:${ROOT}/" \
@@ -125,7 +119,6 @@ if find "${ROOT}/${release_path}/blobs/" -type l >/dev/null 2>/dev/null; then
 fi
 
 docker run \
-    --interactive \
     --rm \
     --volume "${FISSILE_CACHE_DIR}":/bosh-cache \
     --volume "${ROOT}/:${ROOT}/" \
